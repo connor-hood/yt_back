25f2c956d5a188e03efd57659d6c7b7355ebb232
Cdwright93 <80175712+whatupbrownbear@users.noreply.github.com>
2021-05-26 17:18:22 -0400

    fixed serializer

--- youtube_backend/comments/serializers.py
+++ youtube_backend/comments/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Comment
 
 
-class CommentSerializer(serializers.Serializer):
+class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['id', 'user', 'comment']
