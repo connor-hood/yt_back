6499664a4f46e3eb86273ddd765da63035224c7e
Connor Hood <connor_hood@yahoo.com>
2021-05-28 11:07:49 -0500

    got server up and running, added Video ID to comment model and migrated changes

--- youtube_backend/comments/models.py
+++ youtube_backend/comments/models.py
@@ -5,5 +5,6 @@ from django.db import models
 class Comment(models.Model):
     user = models.CharField(max_length=50)
     comment = models.TextField(max_length=300)
+    video_id = models.SlugField()
 
 # add models.SlugField() or models.UUIDField()?
