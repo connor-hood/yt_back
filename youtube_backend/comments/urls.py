f637d93b45e4e45d533f3f9d906b57f9bba2592c
Connor Hood <connor_hood@yahoo.com>
2021-05-28 12:32:58 -0500

    added blank url redirect

--- youtube_backend/comments/urls.py
+++ youtube_backend/comments/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
-    path('comment/', views.CommentView.as_view())
+    path('comment/', views.CommentView.as_view()),
+    path('', RedirectView.as_view(url='/comment/'))
 ]
